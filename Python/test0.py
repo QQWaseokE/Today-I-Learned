24edfb8b8692a6fd6a0dd6eac338cd98188e5bf5
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-25 15:49:45 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,14 +1,7 @@
-import random
+def hello():
+    print("Hello Python!")
 
-n = random.randint(1, 30)
 
-while True:
-    x = input("맞혀 보세요")
-    g = int(x)
-    if g == n:
-        print("정답")
-        break
-    elif g < n:
-        print("up")
-    elif g > n:
-        print("down")
+hello()
+hello()
+hello()
