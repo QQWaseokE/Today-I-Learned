f62488a32fbecc4086e28788fc250c03001bdcb0
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-25 20:56:34 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,6 +1,15 @@
-def hello(name):
-    print("Hello", name)
+def square(a):
+    c = a * a
+    return c
 
 
-hello("waseoke")
-hello("Sheet")
+def triangle(a, h):
+    c = a * h / 2
+    return c
+
+
+s1 = 4
+s2 = square(s1)
+print(s1, s2)
+
+print(triangle(3, 4))
