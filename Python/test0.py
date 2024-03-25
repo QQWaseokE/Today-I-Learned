f4630d54c5c73bcff87e07e908fd15025c5b4396
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-25 21:37:45 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,24 +1,14 @@
 import turtle as t
 
+t.bgcolor("black")
+t.speed(0)
 
-def polygon(n):
-    for x in range(n):
-        t.fd(50)
-        t.left(360 / n)
-
-
-def polygon2(n, a):
-    for x in range(n):
-        t.fd(a)
-        t.left(360 / n)
-
-
-polygon(3)
-polygon(5)
-
-t.up()
-t.fd(100)
-t.down()
-
-polygon2(3, 75)
-polygon2(5, 100)
+for x in range(200):
+    if x % 3 == 0:
+        t.color("red")
+    elif x % 3 == 1:
+        t.color("yellow")
+    elif x % 3 == 2:
+        t.color("blue")
+    t.fd(x * 2)
+    t.left(119)
