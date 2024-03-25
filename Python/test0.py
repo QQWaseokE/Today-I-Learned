0f6070d87eedf0c39022bd3023099c89438e1a27
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-25 15:47:37 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,6 +1,14 @@
-s = 0
-x = 1
-while x <= 10:
-    s = s + x
-    print(x, s)
-    x += 1
+import random
+
+n = random.randint(1, 30)
+
+while True:
+    x = input("맞혀 보세요")
+    g = int(x)
+    if g == n:
+        print("정답")
+        break
+    elif g < n:
+        print("up")
+    elif g > n:
+        print("down")
