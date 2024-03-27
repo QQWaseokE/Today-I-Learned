a50b384b031e635d029cafe3809c1d7b826b3f88
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-27 14:10:17 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,6 +1,9 @@
 import turtle as t
 import random
 
+score = 0
+playing = False
+
 te = t.Turtle()
 te.shape("turtle")
 te.color("red")
@@ -32,19 +35,51 @@ def turn_down():
     t.setheading(270)
 
 
+def start():
+    global playing
+    if playing == False:
+        playing = True
+        t.clear()
+        play()
+
+
 def play():
+    global score
+    global playing
     t.forward(10)
-    ang = te.towards(t.pos())
-    te.setheading(ang)
-    te.forward(9)
+    if random.randint(1, 5) == 3:
+        ang = te.towards(t.pos())
+        te.setheading(ang)
+    speed = score + 5
+
+    if speed > 15:
+        speed = 15
+    te.forward(speed)
+    if t.distance(te) < 12:
+        text = "Score :" + str(score)
+        message("Game Over", text)
+        playing = False
+        score = 0
     if t.distance(ts) < 12:
+        score = score + 1
+        t.write(score)
         star_x = random.randint(-230, 230)
         star_y = random.randint(-230, 230)
         ts.goto(star_x, star_y)
-    if t.distance(te) >= 12:
+    if playing:
         t.ontimer(play, 100)
 
 
+def message(m1, m2):
+    t.clear()
+    t.goto(0, 100)
+    t.write(m1, False, "center", (",20"))
+    t.goto(0, -100)
+    t.write(m2, False, "center", (",15"))
+    t.home()
+
+
+t.title("Turtle Run")
 t.setup(500, 500)
 t.bgcolor("orange")
 t.shape("turtle")
@@ -55,6 +90,7 @@ t.onkeypress(turn_right, "Right")
 t.onkeypress(turn_up, "Up")
 t.onkeypress(turn_left, "Left")
 t.onkeypress(turn_down, "Down")
+t.onkeypress(start, "space")
 t.listen()
-play()
+message("Turtle Run", "[Space]")
 t.mainloop()
