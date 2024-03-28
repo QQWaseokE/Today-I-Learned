9c37325cf521b76ad4b48b3637e9dc4d7e1cb69e
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-28 23:48:41 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,19 +1,13 @@
-# 두 번 이상 나온 이름 찾기
-# 입력 : 이름이 n개 들어 있는 리스트
-# 출력 : 이름 n개 중 반복되는 이름의 집합
-
-
-def find_same_name(a):
+def connect_friend(a):
     n = len(a)
-    result = set()
+
     for i in range(0, n - 1):
         for j in range(i + 1, n):
-            if a[i] == a[j]:
-                result.add(a[i])
-    return result
+            print(a[i], "-", a[j])
 
 
-name = ["Tom", "Jerry", "Mike", "Tom"]
-print(find_same_name(name))
-name2 = ["Tom", "Jerry", "Mike", "Tom", "Mike"]
-print(find_same_name(name2))
+name = ["Tom", "Jerry", "Mike"]
+print(connect_friend(name))
+print()
+name2 = ["Tom", "Jerry", "Mike", "John"]
+print(connect_friend(name2))
