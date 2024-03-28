6fd3bbfbc05579cce5542366536da1030aa68106
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-28 21:03:46 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,8 +1,12 @@
-def sum_n(n):
-    sum = 0
-    for x in range(1, n + 1):
-        sum = sum + x * x
-    return sum
+def find_min(a):
+    n = len(a)
+    min = a[0]
+
+    for x in range(1, n):
+        if min > a[x]:
+            min = a[x]
+    return min
 
 
-print(sum_n(10))
+v = [17, 92, 18, 33, 58, 7, 33, 42]
+print(find_min(v))
