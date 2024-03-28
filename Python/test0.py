0dc96c89106e42d6f452515e8c9204ccd3541ef4
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-28 15:24:42 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,17 +1,8 @@
 def sum_n(n):
-    sum = n * (n + 1) / 2
-
+    sum = 0
+    for x in range(1, n + 1):
+        sum = sum + x * x
     return sum
 
 
 print(sum_n(10))
-
-
-def sum_n2(n):
-    sum = 0
-    for x in range(1, n + 1):
-        sum = sum + x
-    return sum
-
-
-print(sum_n2(10))
