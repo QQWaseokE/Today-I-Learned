abcafb80b18bfa2b6bd8a90ee2caa0d25adb2a89
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-03-31 09:45:51 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,9 +1,7 @@
 def fact(a):
-    if a == 1:
-        return 1
-    if a == 0:
-        return 0
-    return fact(a - 1) + fact(a - 2)
+    if a <= 1:
+        return a
+    return fact(a - 2) + fact(a - 1)
 
 
 print(fact(10))
