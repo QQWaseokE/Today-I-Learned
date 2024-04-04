2875e61c12672788605747540c9004c20ae16ce2
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-04-04 11:10:36 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,19 +1,15 @@
-# 리스트에서 특정 숫자의 위치 찾기
-# 입력 ; 리스트 a, 찾는 값 x
-# 출력 : 찾으면 그 값의 위치, 찾지 못하면 -1
-
-
-def search_list(a, x):
-    list = []
+def search_list(a, b, x):
     n = len(a)
     for i in range(0, n):
         if x == a[i]:
-            list.append(i)
+            return stu_name[i]
 
-    return list
+    return "?"
 
 
-v = [17, 92, 18, 33, 58, 7, 33, 42]
-print(search_list(v, 18))
-print(search_list(v, 33))
-print(search_list(v, 900))
+stu_no = [39, 14, 67, 105]
+stu_name = ["Justin", "John", "Mike", "Summer"]
+
+print(search_list(stu_no, stu_name, 39))
+print(search_list(stu_no, stu_name, 67))
+print(search_list(stu_no, stu_name, 100))
