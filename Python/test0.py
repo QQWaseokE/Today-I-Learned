a6a720b67ce1a138ac2aef7662244f267234b81d
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-04-27 00:06:52 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,20 +1,13 @@
-def fine_min_dix(a):
-    n = len(a)
-    min_idx = 0
-    for i in range(1, n):
-        if a[i] < a[min_idx]:
-            min_idx = i
-    return min_idx
-
-
 def sel_sort(a):
-    result = []
-    while a:
-        min_idx = fine_min_dix(a)
-        value = a.pop(min_idx)
-        result.append(value)
-    return result
+    n = len(a)
+    for i in range(0, n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if a[j] < a[min_idx]:
+                min_idx = j
+        a[i], a[min_idx] = a[min_idx], a[i]
 
 
 d = [2, 4, 5, 1, 3]
-print(sel_sort(d))
+sel_sort(d)
+print(d)
