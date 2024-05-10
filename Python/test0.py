a3bdcd7c2f56e4ef2174694859a90c25e95bebf3
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-05-10 16:34:17 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,20 +1,14 @@
-def find_ins_idx(r, v):
-    for i in range(0, len(r)):
-        if v < r[i]:
-            return i
-    return len(r)
-
-
 def ins_sort(a):
-    result = []
-    while a:
-        value = a.pop(0)  # 리스트에 남은 자료 중 맨 앞의 값을 뽑아냄
-        ins_idx = find_ins_idx(result, value)
-        result.insert(ins_idx, value)  # 찾은 위치에 값 삽입(이후 값은 한 칸씩 밀려남)
-
-        print(a, result)  # 결과 중간 확인
-    return result
+    n = len(a)
+    for i in range(1, n):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] < key:
+            a[j + 1] = a[j]
+            j -= 1
+            a[j + 1] = key
 
 
 d = [2, 4, 5, 1, 3]
-print(ins_sort(d))
+ins_sort(d)
+print(d)
