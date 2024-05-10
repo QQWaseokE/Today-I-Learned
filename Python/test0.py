7e57277e88177c719ac490608f1a01a4ca0fc0de
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-05-10 17:13:54 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,14 +1,40 @@
-def ins_sort(a):
+def merge_sort(a):
     n = len(a)
-    for i in range(1, n):
-        key = a[i]
-        j = i - 1
-        while j >= 0 and a[j] < key:
-            a[j + 1] = a[j]
-            j -= 1
-            a[j + 1] = key
+
+    if n <= 1:
+        return a
+
+    mid = n // 2
+    g1 = a[:mid]
+    g2 = a[mid:]
+    merge_sort(g1)
+    merge_sort(g2)
+
+    i1 = 0
+    i2 = 0
+    ia = 0
+
+    while i1 < len(g1) and i2 < len(g2):
+        if g1[i1] > g2[i2]:
+            a[ia] = g1[i1]
+            i1 += 1
+            ia += 1
+        else:
+            a[ia] = g2[i2]
+            i2 += 1
+            ia += 1
+
+    while i1 < len(g1):
+        a[ia] = g1[i1]
+        i1 += 1
+        ia += 1
+
+    while i2 < len(g2):
+        a[ia] = g2[i2]
+        i2 += 1
+        ia += 1
 
 
-d = [2, 4, 5, 1, 3]
-ins_sort(d)
+d = [6, 8, 3, 9, 10, 1, 2, 4, 7, 5]
+merge_sort(d)
 print(d)
