33ecd8c1434b14b0961d4df765a3e43c9be01d3c
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-05-14 15:35:45 +0900

    .

--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-print((max(x) - min(x)) * (max(y) - min(y)))
+ # if x == a[mid]:
+    #     return mid
+    # elif x > a[mid]:
+    #     binary_search(a[mid:], x)
+    # else:
+    #     binary_search(a[:mid], x)
+
+    # return -1
--- Python/test0.py
+++ Python/test0.py
@@ -1,22 +1,19 @@
-# 리스트에서 특정 숫자 위치 찾기(이분 탐색)
-# 입력 : 리스트 a, 찾는 값 x
-# 출력 : 찾으면 그 값의 위치, 찾지 못하면 -1
+def binary_search_sub(a, x, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+
+    if x == a[mid]:
+        return mid
+    elif x > a[mid]:
+        return binary_search_sub(a, x, mid + 1, end)
+    else:
+        return binary_search_sub(a, x, start, mid - 1)
 
 
 def binary_search(a, x):
-    start = 0
-    end = len(a) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-        if x == a[mid]:
-            return mid
-        elif x > a[mid]:
-            start = mid + 1
-        else:
-            end = mid - 1
-
-    return -1
+    return binary_search_sub(a, x, 0, len(a) - 1)
 
 
 d = [1, 4, 9, 16, 25, 36, 49, 64, 81]
