e1ddcfaaf2c21177af584acc0eb7e89c05227333
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-06-02 21:28:09 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,12 +1,14 @@
 def max_profit(prices):
     n = len(prices)
     max_profit = 0
+    min_price = prices[0]
 
-    for i in range(0, n - 1):
-        for j in range(i + 1, n):
-            profit = prices[j] - prices[i]
-            if profit > max_profit:
-                max_profit = profit
+    for i in range(1, n):
+        profit = prices[i] - min_price
+        if profit > max_profit:
+            max_profit = profit
+        if prices[i] < min_price:
+            min_price = prices[i]
 
     return max_profit
 
