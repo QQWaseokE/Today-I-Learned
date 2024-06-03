63ebc5d6afe1e3c61f8d5bede29d8cfa4ecb7796
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-06-03 13:41:15 +0900

    Update test0.py

--- Python/test0.py
+++ Python/test0.py
@@ -1,4 +1,20 @@
-def max_profit(prices):
+import time
+import random
+
+
+def max_profit_slow(prices):
+    n = len(prices)
+    max_profit = 0
+
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            profit = prices[j] - prices[i]
+            if profit > max_profit:
+                max_profit = profit
+    return max_profit
+
+
+def max_profit_fast(prices):
     n = len(prices)
     max_profit = 0
     min_price = prices[0]
@@ -9,9 +25,37 @@ def max_profit(prices):
             max_profit = profit
         if prices[i] < min_price:
             min_price = prices[i]
-
     return max_profit
 
 
-stock = [10300, 9600, 9800, 8200, 7800, 8300, 9500, 9800, 10200, 9500]
-print(max_profit(stock))
+def test(n):  # n = 주가 개수
+    a = []
+    for i in range(0, n):
+        a.append(random.randint(5000, 20000))  # 주가로 사용
+
+    # 느린 O(n*n) 알고리즘 테스트
+    start = time.time()
+    mps = max_profit_slow(a)
+    end = time.time()
+    time_slow = end - start
+
+    # 빠른 O(n) 알고리즘 테스트
+    start = time.time()
+    mpf = max_profit_fast(a)
+    end = time.time()
+    time_fast = end - start
+
+    # 결과 출력 : 계산 결과 (mps와 mpf가 같아야 함)
+    print(n, mps, mpf)
+
+    # 결과 출력 : 계산 시간 비교
+    m = 0
+    if time_fast > 0:
+        m = time_slow / time_fast
+
+    print("%d %.5f %.5f %.2f" % (n, time_slow, time_fast, m))
+
+
+test(100)
+test(10000)
+test(100000)
